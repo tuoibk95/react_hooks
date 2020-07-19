0c71e0e4c8180f9a8187c0c5d7013b47c62417a5
tuoibk95 <tuoith95@gmail.com>
2020-07-19 23:09:43 +0700

    commit use state hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './app.scss';
 
 function App() {
+    const [count, setCount] = useState(0);
+    
   return (
     <div className="App">
       <header className="App-header">
+      <div className="app">
+        Lê Văn Tươi
+        </div>
+        <div>
+            <p>You cliked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+            <button onClick={() => setCount(count - 1)}>ClickDown</button>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,9 +27,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <div className="app">
-        Lê Văn Tươi
-      </div>
+      
     </div>
   );
 }
